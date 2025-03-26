a1432c3bc2e459d702d359c0e732c5e244fa4d32
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-03-26 13:06:50 -0300

    feat(urls) configracion de urls: - registro de url de RegisterView en users/urls - registro de url de users en medical/urls

---
+++ users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import RegisterView, CustomTokenObtainPairView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register')
+]
