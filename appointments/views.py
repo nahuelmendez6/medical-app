8cf5ec6558ff651852ec3d76a85c13691864351e
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-03-29 17:25:18 -0300

    feat(appointments): Implementacion de CreateAppointmentView y registro de su url correspondiente

--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
+from .serializers import AppointMentSerializer
 # Create your views here.
+
+class CreateAppointmentView(APIView):
+
+    permission_classes = [AllowAny]
+    """
+    Endpoint para registar una cita
+    """
+    def post(self, request):
+        serializer = AppointMentSerializer(data=request.data)
+        if serializer.is_valid():
+            appointment = serializer.save()
+            return Response(
+                {'message':'Cita creada exitosamente'},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
