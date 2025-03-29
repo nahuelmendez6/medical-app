aa43f2714ce1e8b935a4a096d1272d81b2d76ce3
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-03-29 00:32:52 -0300

    feat(urls): registro de urls para CrateDoctorProfile, DoctorSchedule y CreatePatientProfile

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import RegisterView, CustomTokenObtainPairView, CreateDoctorProfileView
+from .views import (RegisterView, CustomTokenObtainPairView, CreateDoctorProfileView, CreateDoctorScheduleView,
+                    CreatePatientProfileView)
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register')
+    path('register/', RegisterView.as_view(), name='register'),
+    path('register/profile/doctor', CreateDoctorProfileView.as_view(), name='doctor_profile'),
+    path('register/schedule/doctor', CreateDoctorScheduleView.as_view(), name='doctor_schedule'),
+    path('register/patient/profile', CreatePatientProfileView.as_view(), name='patient_profile')
 ]
