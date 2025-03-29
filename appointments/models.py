1582474c3f32594ff108643cded15d0876acd270
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-03-29 16:18:56 -0300

    feat(appointments): Implementacion de los modelos Appointments y AppointmentsSchedule

--- appointments/models.py
+++ appointments/models.py
@@ -1,8 +1,31 @@
 from django.db import models
+from django.db.models import UniqueConstraint
+
+from users.models import CustomUser, DoctorProfile, PatientProfile
+
 
 # Create your models here.
-class AppointmentSchedule(models.Model):
-    pass
+
 
 class Appointments(models.Model):
-    pass
+    STATUS_CHOICES = (
+        ('pending', 'Pendiente'),
+        ('confirmed', 'Confirmado'),
+        ('cancelled', 'Candelada'),
+        ('completed', 'Completada')
+    )
+    patient = models.ForeignKey(PatientProfile, on_delete=models.CASCADE, related_name='appointments_patient')
+    doctor = models.ForeignKey(DoctorProfile, on_delete=models.CASCADE, related_name='appointments_doctor')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['doctor', 'date', 'start_time'], name='unique_appointment')
+        ]
+
+class AppointmentSchedule(models.Model):
+
+    appointment = models.ForeignKey(Appointments, on_delete=models.CASCADE, related_name='appointment_date')
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
